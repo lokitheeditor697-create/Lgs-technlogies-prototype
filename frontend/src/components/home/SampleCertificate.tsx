b7a016be5d33e45e741a28e5a7fed2472dd9f0cb
LGS Technologies <lgstechnologiess@gmail.com>
2026-07-22 21:12:48 +0530

    Update master template image and fine-tune signature alignment

--- frontend/src/components/home/SampleCertificate.tsx
+++ frontend/src/components/home/SampleCertificate.tsx
@@ -84,7 +84,7 @@ export default function SampleCertificate() {
                 </div>
 
                 {/* Official Black Ink Signature Overlay */}
-                <div className="absolute bottom-[14%] left-[45%] z-20 w-[18%] aspect-auto pointer-events-none select-none">
+                <div className="absolute bottom-[13%] left-[48%] z-20 w-[16%] aspect-auto pointer-events-none select-none">
                   <img 
                     src="/images/signature.png" 
                     alt="Authorized Signature" 
